605b2c7ea988d5f74fae693263f26707f82dc177
Gabriel Diegel <gabriel.diegel@icloud.com>
2021-08-05 12:03:35 -0500

    Prepare for Docker

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
-const hostname = "127.0.0.1"
-const port = 3000
+const hostname = "0.0.0.0"
+const port = 8080
 
 function col(num, arr) {
     arr.push(num)
