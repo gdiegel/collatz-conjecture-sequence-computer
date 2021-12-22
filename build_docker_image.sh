e38a733b56158c6e7949c481a8c98c91b42d7c13
Gabriel Diegel <gabriel.diegel@icloud.com>
2021-12-22 11:55:26 -0500

    Exchange registry for GCP

--- build_docker_image.sh
+++ build_docker_image.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -ex
 
-REGISTRY=collatz.azurecr.io
+REGISTRY=us-east1-docker.pkg.dev/example-1615678616955/collatz
 IMAGE=collatz-sequence-computer-service
 TAG=develop
 docker build -t $REGISTRY/$IMAGE:$TAG .
